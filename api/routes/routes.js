e3acc6616b670b7a219e0f1512d8f2ebd952abbb
RianAsmara <gagahkalem@gmail.com>
2019-05-20 00:55:30 +0800

    Update JWT

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,21 @@
 // Initialize express router
 let router = require('express').Router();
 
+// Import morgan
+// let morgan = require('morgan');
+
+// Import JWT
+// jwt = require('jsonwebtoken');
+
+// Import Config
+// let config = require('../../config/config');
+
+// Set secret
+// app.set('Secret', config.secret);
+
+// Use morgan
+// app.use(morgan('dev'));
+
 // Default API response
 router.get('/', function(req, res) {
     res.json({
@@ -22,5 +37,58 @@ router.route('/contacts/:contact_id')
     .patch(contactController.update)
     .put(contactController.update)
     .delete(contactController.delete);
+
+// Route Auth
+// router.post('/authenticate', function(req, res) {
+//     if (req.body.username=="imnothing"){
+//         if (req.body.password==1607){
+//             // Create token
+//             const payload = {
+//                 check: true
+//             };
+//             var token = jwt.sign(payload, app.get('Secret'), {
+//                 expiresIn: 1440 //24 Hours
+//             });
+//             res.json({
+//                 message: 'authentication done',
+//                 token: token
+//             });
+//         }else{
+//             res.json({
+//                 message:'please check your password'
+//             });
+//         }
+//     }else{
+//         res.json({
+//             message: 'user not found'
+//         })
+//     }
+// });
+
+// Middleware
+// router.use((req, res, next) => {
+//     // Check header for token
+//     var token = req.headers['access-token'];
+
+//     // Decode tokne
+//     if (token) {
+//         // Validate and check if token is expired
+//         jwt.verify(token, app.get('Secret'), (err, decoded) => {
+//             if (err) {
+//                 return res.json({
+//                     message: 'invalid token'
+//                 });
+//             }else{
+//                 req.decoded = decoded;
+//                 next();
+//             }
+//         });
+//     }else{
+//         // If there's no token
+//         res.send({
+//             message: 'No Token Provided.'
+//         });
+//     }
+// });
 
 module.exports = router;
--- index.js
+++ index.js
@@ -10,6 +10,21 @@ let mongoose = require('mongoose');
 // Initialize app
 let app = express();
 
+// // Import morgan
+// let morgan = require('morgan');
+
+// // Import JWT
+// jwt = require('jsonwebtoken');
+
+// // Import Config
+// let config = require('./config/config');
+
+// // Set secret
+// app.set('Secret', config.secret);
+
+// // Use morgan
+// app.use(morgan('dev'));
+
 // Import routes
 let apiRoutes = require("./api/routes/routes");
 
@@ -28,7 +43,7 @@ var db = mongoose.connection;
 var port = process.env.port || 8888;
 
 // Send response
-app.get('/', (req, res) => res.send('Hello From Express and Nodemon'));
+app.get('/', (req, res) => res.send('Hello From Express'));
 
 // Use API Routes
 app.use('/api', apiRoutes);
